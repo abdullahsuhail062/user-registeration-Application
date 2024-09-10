34f2e1ef5049197c6b4b4d0b7a8fa165e08d4873
abdullahsuhail062 <abdullahsuhail062@gmail.com>
2024-09-10 16:24:41 +0500

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -62,11 +62,12 @@ export class AppComponent {
 handleValidationErrors(errors: any[]) {
   this.formErrors = {}; // Clear previous errors
   errors.forEach(err => {
-    if (err.param === 'username') {
-      this.formErrors.username = err.msg; // Assign error message to the username
+    if (err.includes('Username must be at least 3 characters long')) {
+      this.formErrors.username = err // Assign error message to the username
+      console.log(this.formErrors.username);
+
     }
     //this.formErrors[err.param] = err.msg; // Associate errors with form controls
-    console.log(this.formErrors);
     
   });   
   }
