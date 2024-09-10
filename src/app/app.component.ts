cad7e7c219357a4726094568c88940fd76ec7cc3
abdullahsuhail062 <abdullahsuhail062@gmail.com>
2024-09-10 22:42:08 +0500

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,7 +24,7 @@ import { AsyncCompleter } from 'node:readline';
 @Injectable({providedIn: 'root'})
 export class AppComponent {
   signUpForm: FormGroup
-  formErrors: any = {userName: ''};
+  formErrors: any = {emptyUserNameHandler: '', charactersLengthUsernameHandler: ''};
   constructor(private http: HttpClient){
     this.signUpForm = new FormGroup({username: new FormControl('',[Validators.required,Validators.pattern('^[a-zA-Z0-9]*')]), email: new FormControl('',[Validators.required, Validators.pattern(/^[a-zA-Z0-9._%+-]+@example\.(com|org|net)$/)]),password: new FormControl('',[Validators.required,Validators.minLength(6),Validators.pattern('^[a-zA-Z0-9]*')]),confirmPassword: new FormControl('',[Validators.required,Validators.minLength(6)])})
   }
@@ -62,8 +62,13 @@ export class AppComponent {
 handleValidationErrors(errors: any[]) {
   this.formErrors = {}; // Clear previous errors
   errors.forEach(err => {  
-    this.formErrors.userName = err.msg // Assign error message to the username
-    console.log(this.formErrors.userName);
+    if (err.param === 'Please provide the username') {
+      this.formErrors.emptyUserNameHandler = err.msg // Assign error message to the username
+      
+    } else if(err.param === 'Username must be at least 3 characters long'){
+      this.formErrors.charactersLengthUsernameHandler = err.msg
+    }
+   
     
 
     // if (err.param ==='username') {
