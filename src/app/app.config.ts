aa4a98aff531739279b8f68f436109687bc7a3d4
abdullahsuhail062 <abdullahsuhail062@gmail.com>
2024-09-14 02:30:29 +0500

    update

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,8 +6,8 @@ withFetch()
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { HttpClient, provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideAnimationsAsync(), provideAnimationsAsync(), provideHttpClient( withFetch(),),]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideAnimationsAsync(), provideAnimationsAsync(), provideHttpClient( withFetch()),]
 };
