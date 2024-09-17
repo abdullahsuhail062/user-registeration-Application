5b9978043a8c8990f2c58bbceac347d22825344f
abdullahsuhail062 <abdullahsuhail062@gmail.com>
2024-09-17 22:45:26 +0500

    update

--- src/app/api-service.service.ts
+++ src/app/api-service.service.ts
@@ -19,30 +19,30 @@ formData: {username: string} ={username: 'Abu Abdullah'}
     
     return this.http.post<any>('apiUrl/api/register', this.formData, { responseType: 'json' })
     
-//     .pipe(
-//       catchError(this.handleError)  // Catch errors here
-//     );
-// }
+    .pipe(
+      catchError(this.handleError)  // Catch errors here
+    );
+}
 
-// // Error handling function
-// private handleError(error: HttpErrorResponse) {
-//   let errorMessage = '';
+// Error handling function
+private handleError(error: HttpErrorResponse) {
+  let errorMessage = '';
 
-//   if (error.error instanceof ErrorEvent) {
-//     // Client-side or network error occurred
-//     errorMessage = `Client-side error: ${error.error.message}`;
-//   } else if (error.error ===null) {
-//         // Server-side error occurred
-//     errorMessage = `Server returned code: ${error.status}, error message is: ${error.message}`;
+  if (error.error instanceof ErrorEvent) {
+    // Client-side or network error occurred
+    errorMessage = `Client-side error: ${error.error.message}`;
+  } else if (error.error ===null) {
+        // Server-side error occurred
+    errorMessage = `Server returned code: ${error.status}, error message is: ${error.message}`;
   
-//   } {
-//     }
+  } {
+    }
 
-//   // Log the error (you can also use more advanced logging mechanisms)
-//   console.error(errorMessage);
+  // Log the error (you can also use more advanced logging mechanisms)
+  console.error(errorMessage);
 
-//   // Return a user-friendly message or rethrow the error
-//   return throwError(() => new Error(errorMessage));
+  // Return a user-friendly message or rethrow the error
+  return throwError(() => new Error(errorMessage));
 }
 }
   
