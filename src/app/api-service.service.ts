11ede44176d6b25b4169d7dfd802cb1240b8fe3e
abdullahsuhail062 <abdullahsuhail062@gmail.com>
2024-09-17 15:57:10 +0500

    update

--- src/app/api-service.service.ts
+++ src/app/api-service.service.ts
@@ -1,12 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { environment } from './environments/environment.prod';  // Make sure this path is correct
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiServiceService {
+ environment ={production: true, apiUrl: 'https://user-registeration-server-app.vercel.app' }
+
   private apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) {}
