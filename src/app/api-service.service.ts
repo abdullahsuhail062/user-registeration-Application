6c8fce0eb63ef45703722ed0329c2dda634cb34f
abdullahsuhail062 <abdullahsuhail062@gmail.com>
2024-09-17 15:35:29 +0500

    update

--- src/app/api-service.service.ts
+++ src/app/api-service.service.ts
@@ -1,19 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { environment } from './environment.prod';
-import {  Observable } from 'rxjs';
-
+import { environment } from './environments/environment.prod';  // Make sure this path is correct
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiServiceService {
-private apiUrl = environment.apiUrl
-  constructor(private http: HttpClient) {}
-  
-registerUser(formData: {username: string})  {
-  const url = `${this.apiUrl}/api/register`
-  return this.http.post(url, formData, {responseType: 'json'})
-}
+  private apiUrl = environment.apiUrl;
 
+  constructor(private http: HttpClient) {}
+
+  registerUser(formData: { username: string }): Observable<any> {
+    const url = `${this.apiUrl}/api/register`;
+    return this.http.post<any>(url, formData, { responseType: 'json' });
+  }
 }
