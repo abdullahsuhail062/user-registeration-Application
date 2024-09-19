30a224e65c372af197c3d724bd58af9d2a4d1df6
abdullahsuhail062 <abdullahsuhail062@gmail.com>
2024-09-19 13:44:41 +0500

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -40,7 +40,7 @@ export class AppComponent {
     this.apiService.registerUser().subscribe({next: (data) => {console.log(data.message);
     }, error: (error) => {
       
-        console.log(error.errorMessage);
+        console.error(error.errorMessage);
         
       
       
