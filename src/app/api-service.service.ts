115cd36473d0685ec8ce939aec9b832bd2437cfe
abdullahsuhail062 <abdullahsuhail062@gmail.com>
2024-09-19 14:36:58 +0500

    update

--- src/app/api-service.service.ts
+++ src/app/api-service.service.ts
@@ -10,7 +10,7 @@ import { environment } from './environments/environment.prod';
 })
 export class ApiServiceService {
 formData: {username: string} ={username: 'Abu Abdullah'}
-  private apiUrl = 'https://user-registeration-ap.server.vercel.app/api/register'
+  private apiUrl = 'https://user-registeration-server.app.vercel.app/api/register'
 
   constructor(private http: HttpClient) {}
   
