b8265154d4f8646e0e02d8eca53b118d95ec8f53
abdullahsuhail062 <abdullahsuhail062@gmail.com>
2024-10-12 11:18:35 +0500

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -37,7 +37,7 @@ export class AppComponent {
   constructor(private apiService: ApiServiceService){
     this.signUpForm = new FormGroup({username: new FormControl('',[Validators.required,Validators.minLength(3),Validators.pattern('^[a-zA-Z0-9]*')])
       ,email: new FormControl('',[Validators.required, Validators.pattern(/^[a-zA-Z0-9._%+-]+@example\.(com|org|net)$/)]),password: new FormControl('',[Validators.required,Validators.minLength(8),
-        Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$'
+        Validators.pattern('/^[a-zA-Z0-9]+$/'
 )]),confirmPassword: new FormControl('',[Validators.required,Validators.minLength(8)])}) 
     }
 
