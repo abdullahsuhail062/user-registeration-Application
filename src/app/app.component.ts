3d1f80f5c8e979723e339bd8eef9e96cc032bb3d
abdullahsuhail062 <abdullahsuhail062@gmail.com>
2024-10-14 01:56:44 +0500

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -52,7 +52,7 @@ export class AppComponent {
     const password = this.signUpForm.get('password')?.value
     this.passwordsMisMatchValidator()
     
-    if (this.signUpForm.invalid) {
+    if (this.signUpForm.valid) {
 
     this.apiService.registerUser({username,email,password}).subscribe({next: (data) => {console.log(data.message);
     }, error: (error) => {
--- src/app/errorInceptor.ts
+++ src/app/errorInceptor.ts
@@ -12,12 +12,7 @@ function  handleError(error: HttpErrorResponse): Observable<never>{
       const errorMessage = error.error.errors
       console.log(errorMessage);
       
-      return throwError(()=> (errorMessage))
-    }else if (error.error.errors ===400 && error.error.errors === 'Request body is empty') {
-      console.log(error.error.errors);
-      
-      
-    }
+      return throwError(()=> (errorMessage}
       if (error.error instanceof ErrorEvent){
           console.log('client side error', error.error);
           
