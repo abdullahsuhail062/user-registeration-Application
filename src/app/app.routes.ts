40db20f114edb2038da452dbe924edf0602a4d11
abdullahsuhail062 <abdullahsuhail062@gmail.com>
2024-11-08 20:41:26 +0500

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { RegisterationComponent } from './registeration/registeration.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [{path: '', component: RegisterationComponent},
-     {path: 'dashboard', component: DashboardComponent},];
+     {path: 'dashboard', component: DashboardComponent},{path: 'login', component: LoginComponent}];
