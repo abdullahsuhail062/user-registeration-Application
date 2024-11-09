9116b17e297c7844f670b250109fe57eec3689c1
abdullahsuhail062 <abdullahsuhail062@gmail.com>
2024-11-09 10:13:25 +0500

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './auth.guard';
 import { AppComponent } from './app.component';
 import { RegisterationComponent } from './registeration/registeration.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
 
 export const routes: Routes = [{path: '', component: RegisterationComponent},{path: 'registeration',component: RegisterationComponent},
-     {path: 'dashboard', component: DashboardComponent},{path: 'login', component: LoginComponent}];
+     { path: 'dashboard', component: DashboardComponent, canActivate: [authGuard] },{path: 'login', component: LoginComponent},{ path: '', redirectTo: '/login', pathMatch: 'full' },{ path: '**', redirectTo: '/login' }];
---
+++ src/app/auth.guard.ts
@@ -0,0 +1,14 @@
+import { CanActivateFn, Router } from '@angular/router';
+import { inject, Inject } from '@angular/core';
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const router = inject(Router)
+  const token = localStorage.getItem('authToken'); // Check for token in local storage
+
+  if (token) {
+    return true; // Allow access if token exists
+  } else {
+    router.navigate(['/login']); // Redirect to login if no token
+    return false;
+}
+}
