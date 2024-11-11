63c6b3b6d5f59747fbefa65f9467a2359d1b2787
abdullahsuhail062 <abdullahsuhail062@gmail.com>
2024-11-11 15:18:02 +0500

    update

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -7,8 +7,12 @@ export class AuthService {
 
   constructor() { }
   private readonly TOKEN_KEY = 'authToken';
+  
 
   isLoggedIn(): boolean {
+    const extrVal = localStorage.getItem(this.TOKEN_KEY)
+    console.log(extrVal);
+    
     return !!localStorage.getItem(this.TOKEN_KEY); // Check if the token exists
   }
 
