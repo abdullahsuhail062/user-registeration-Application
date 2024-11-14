e0f5a7b2fadd45ea9180efafaa16cd5cee6bc13d
abdullahsuhail062 <abdullahsuhail062@gmail.com>
2024-11-14 15:20:33 +0500

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,5 +6,5 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 
-export const routes: Routes = [{path: '', component: RegisterationComponent},{path: 'registeration',component: RegisterationComponent},
-     { path: 'dashboard', component: DashboardComponent, canActivate: [authGuard] },{path: 'login', component: LoginComponent, canActivate: [loginGuard]},{ path: '', redirectTo: '/login', pathMatch: 'full' },{path: 'user-profile', component: UserProfileComponent},{path: 'app.component', component: AppComponent}];
+export const routes: Routes = [{path: 'registeration',component: RegisterationComponent},
+     { path: 'dashboard', component: DashboardComponent, canActivate: [authGuard] },{path: 'login', component: LoginComponent, canActivate: [loginGuard]},{ path: '', redirectTo: '/login', pathMatch: 'full' },{path: 'user-profile', component: UserProfileComponent},{path: '', component: AppComponent}];
