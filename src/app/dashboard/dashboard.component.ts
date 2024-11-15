594c10d48aa5eafce417827a770a456992865e7d
abdullahsuhail062 <abdullahsuhail062@gmail.com>
2024-11-15 21:41:08 +0500

    update

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -38,7 +38,7 @@ export class DashboardComponent implements OnInit {
   
   openProfileDialog(): void{
     this.apiService.fetchUserProfile().subscribe({next: (userdata)=>{
-      this.dialog.open(UserProfileComponent,{position: {top: '20px', right: '20px'},data:{username: userdata.username, email: userdata.email, onLogout: () => this.logout(), onNavigateToDashboard: () => this.dashboard()}})
+      this.dialog.open(UserProfileComponent,{position: {top: '25px', right: '20px'},data:{username: userdata.username, email: userdata.email, onLogout: () => this.logout(), onNavigateToDashboard: () => this.dashboard()}})
     }, error: (error)=>{console.log(error.error);
     }})
   
