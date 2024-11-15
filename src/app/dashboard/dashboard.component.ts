9000be2c7c9bd2b4e9d1a69fe748d30570910170
abdullahsuhail062 <abdullahsuhail062@gmail.com>
2024-11-15 20:28:30 +0500

    update

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -16,7 +16,8 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 export class DashboardComponent implements OnInit {
   profileInitial: string =''
   email: any
-  constructor(private matDialogCong: MatDialogConfig,private router: Router, private authservice: AuthService, private dialog: MatDialog, private apiService: ApiServiceService){}
+  constructor(priva 
+    : MatDialogConfig,private router: Router, private authservice: AuthService, private dialog: MatDialog, private apiService: ApiServiceService){}
   ngOnInit(): void {
     this.apiService.fetchUserProfile().subscribe({next: (data)=>{
       this.profileInitial = data.email.charAt(0).toUpperCase(); }}) }
@@ -39,19 +40,10 @@ export class DashboardComponent implements OnInit {
   openProfileDialog(): void{
     
 
-    // Set the position to top-right
-    this.matDialogCong.position = {
-      top: '20px', // Distance from the top
-      right: '20px', // Distance from the right
-    };
+    
 
-    // Optional: Set a custom width and height
-    this.matDialogCong.width = '300px';
-    this.matDialogCong.height = 'auto';
-
-    const dialogConfig = this.matDialogCong
     this.apiService.fetchUserProfile().subscribe({next: (userdata)=>{
-      this.dialog.open(UserProfileComponent,{dialogConfig, data:{username: userdata.username, email: userdata.email, onLogout: () => this.logout(), onNavigateToDashboard: () => this.dashboard()}})
+      this.dialog.open(UserProfileComponent,{data:{username: userdata.username, email: userdata.email, onLogout: () => this.logout(), onNavigateToDashboard: () => this.dashboard()},panelClass: 'custom-dialog'})
     }, error: (error)=>{console.log(error.error);
     }})
   
