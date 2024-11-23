1eb1528631b38848bbd1a9ca29c78164ed5151ed
abdullahsuhail062 <abdullahsuhail062@gmail.com>
2024-11-23 23:39:00 +0500

    update

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -48,7 +48,8 @@ export class LoginComponent {
    
     this.passwordsMisMatchValidator()
     if (this.loginForm.valid) {
-    this.apiService.loginUser(formData).subscribe({next: (data) => {if (this.router.url === '/login'){this.isLoading =true}localStorage.setItem('authToken', data.token),this.router.navigate(['/dashboard']);
+    this.apiService.loginUser(formData).subscribe({next: (data) => {if (this.router.url === '/login'){this.isLoading =true,console.log(this.isLoading);
+    }localStorage.setItem('authToken', data.token),this.router.navigate(['/dashboard']);
       const token = data.token; // Assume this is the JWT token from backend
       const expiresAt = Date.now() + 3600 * 1000; // Set expiration time to 1 hour from now
       localStorage.setItem('authToken', token);
