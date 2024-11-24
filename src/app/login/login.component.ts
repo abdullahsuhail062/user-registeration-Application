33815781f416c28ed5352695f51d4369fac31065
abdullahsuhail062 <abdullahsuhail062@gmail.com>
2024-11-24 23:03:52 +0500

    update

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -30,7 +30,7 @@ export class LoginComponent {
   passwordError: any
   mismatchPasswordsError: any
   generalError: any
-  isLoading: boolean = false
+  isLoading: boolean = true
 
   constructor(private router: Router,private apiService: ApiServiceService, private http: HttpClient){
     this.loginForm = new FormGroup({email: new FormControl('',[Validators.required, Validators.pattern(/^[a-zA-Z0-9._%+-]+@example\.(com|org|net)$/)]),password: new FormControl('',[Validators.required,Validators.minLength(8),
