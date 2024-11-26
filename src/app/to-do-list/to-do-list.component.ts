514a997110fd31ccebf743429338c2bf6c0bdaa9
abdullahsuhail062 <abdullahsuhail062@gmail.com>
2024-11-26 07:17:42 +0500

    update

--- src/app/to-do-list/to-do-list.component.ts
+++ src/app/to-do-list/to-do-list.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+ import{MatToolbarModule} from '@angular/material/toolbar'
 
 @Component({
   selector: 'app-to-do-list',
   standalone: true,
-  imports: [],
+  imports: [MatToolbarModule],
   templateUrl: './to-do-list.component.html',
   styleUrl: './to-do-list.component.scss'
 })
