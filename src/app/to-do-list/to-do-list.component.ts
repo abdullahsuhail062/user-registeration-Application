1251dd6fcf06d4270a01dbb6a7e4f31169e828bb
abdullahsuhail062 <abdullahsuhail062@gmail.com>
2024-11-26 20:30:31 +0500

    update

--- src/app/to-do-list/to-do-list.component.ts
+++ src/app/to-do-list/to-do-list.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, TemplateRef } from '@angular/core';
+import { MatDialog, MatDialogActions, MatDialogContent } from '@angular/material/dialog';
  import{MatToolbarModule} from '@angular/material/toolbar'
 
 @Component({
   selector: 'app-to-do-list',
   standalone: true,
-  imports: [MatToolbarModule],
+  imports: [MatToolbarModule, MatDialogContent,MatDialogActions],
   templateUrl: './to-do-list.component.html',
   styleUrl: './to-do-list.component.scss'
 })
 export class ToDoListComponent {
+
+  constructor(private dialog: MatDialog){}
+
+  openDialog(templateRef: TemplateRef<any>): void{
+    this.dialog.open(templateRef,{})
+  }
 
 }
