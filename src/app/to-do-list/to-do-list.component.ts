ed5a32456efa8017b00ad17de493da121564b87b
abdullahsuhail062 <abdullahsuhail062@gmail.com>
2024-11-27 21:10:29 +0500

    update

--- src/app/to-do-list/to-do-list.component.ts
+++ src/app/to-do-list/to-do-list.component.ts
@@ -14,7 +14,7 @@ export class ToDoListComponent {
   constructor(private dialog: MatDialog){}
 
   openDialog(templateRef: TemplateRef<any>): void{
-    this.dialog.open(templateRef,{position:{top:'4%', left: '6%'},width:'300px',height: '400px'})
+    this.dialog.open(templateRef,{position:{top:'4%', left: '8%'},height: '200px'})
   }
 
 }
