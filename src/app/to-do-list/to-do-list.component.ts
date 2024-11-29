9a34d01a6a7f7c13791b1ad97a779bd38b607ae3
abdullahsuhail062 <abdullahsuhail062@gmail.com>
2024-11-29 21:52:33 +0500

    update

--- src/app/to-do-list/to-do-list.component.ts
+++ src/app/to-do-list/to-do-list.component.ts
@@ -1,21 +1,44 @@
+import { NgClass } from '@angular/common';
 import { Component, TemplateRef } from '@angular/core';
-import { MatDialog, MatDialogActions, MatDialogContent } from '@angular/material/dialog';
+import { FormsModule } from '@angular/forms';
+import { MatDialog, MatDialogContent } from '@angular/material/dialog';
 import { MatFormField } from '@angular/material/form-field';
  import{MatToolbarModule} from '@angular/material/toolbar'
 
 @Component({
   selector: 'app-to-do-list',
   standalone: true,
-  imports: [MatToolbarModule, MatDialogContent,MatFormField],
+  imports: [MatToolbarModule, MatDialogContent,FormsModule,NgClass],
   templateUrl: './to-do-list.component.html',
   styleUrl: './to-do-list.component.scss'
 })
 export class ToDoListComponent {
+activateBtn: boolean = true 
+input: string = ''
+isDeactive: boolean = true
+isActive: boolean = false
 
   constructor(private dialog: MatDialog){}
 
   openDialog(templateRef: TemplateRef<any>): void{
     this.dialog.open(templateRef,{position:{top:'4%', left: '11%'},height: '200px'})
+  }
+
+  createList(){
+    if (this.input.length>0) {
+      this.activateBtn = false
+
+      
+    }
+  }
+
+  toggleBtnColor(){
+    if (this.input.length>0) {
+      this.isActive = true
+      
+    }else {this.isDeactive = false}
+
+
   }
 
 }
