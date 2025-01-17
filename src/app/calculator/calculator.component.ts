a8538b93186d991eb64c969ffc2d25b2f82097bd
Your Name <you@example.com>
2025-01-17 07:37:17 +0500

    update

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-calculator',
-  imports: [],
+  imports: [MatToolbarModule],
   templateUrl: './calculator.component.html',
   styleUrl: './calculator.component.css'
 })
 export class CalculatorComponent {
+
+  constructor(private router: Router){}
+  navigateBackToHome(){
+    this.router.navigate(['/dashboard'])
+
+  }
 
 }
