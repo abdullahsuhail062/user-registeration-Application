467f44536c4dfca5b24fbce89c9f76540be1f762
Your Name <you@example.com>
2025-01-18 20:32:36 +0500

    update

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -1,19 +1,27 @@
-import { Component } from '@angular/core';
+import { NgFor } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-calculator',
-  imports: [MatToolbarModule],
+  imports: [MatToolbarModule,NgFor],
   templateUrl: './calculator.component.html',
   styleUrl: './calculator.component.css'
 })
-export class CalculatorComponent {
+export class CalculatorComponent implements OnInit {
+  button: number =0
+  buttons: [button: number] = [this.button]
 
+  
   constructor(private router: Router){}
   navigateBackToHome(){
     this.router.navigate(['/dashboard'])
 
+  }
+
+  ngOnInit(): void {
+    this.buttons.push(this.button)
   }
 
 }
