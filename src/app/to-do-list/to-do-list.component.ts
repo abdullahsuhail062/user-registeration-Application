1710244872a8cf8eaeffed5e3f14da9ef9c2e5d9
Your Name <you@example.com>
2025-02-13 08:47:40 +0500

    update

--- src/app/to-do-list/to-do-list.component.ts
+++ src/app/to-do-list/to-do-list.component.ts
@@ -40,7 +40,7 @@ isTaskExist: boolean= false
   constructor(private router: Router,private authService: AuthService,private dialog: MatDialog, private apiService: ApiServiceService ){}
  ngOnInit(): void {
   const token =this.authService.getToken()
-    this.apiService.getTasks(token).subscribe({next:(tasks)=>{console.log(tasks);
+    this.apiService.getTasks(token).subscribe({next:(tasks)=>{this.items=tasks;
     
     },error:(
       error)=>{this.handleTaskFetchingError(error)}})
