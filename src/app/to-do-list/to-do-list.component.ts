d5c370e3d458bd61daa964884aff624ffa733bc7
Your Name <you@example.com>
2025-02-20 12:31:54 +0500

    update

--- src/app/to-do-list/to-do-list.component.ts
+++ src/app/to-do-list/to-do-list.component.ts
@@ -142,6 +142,9 @@ isTaskExist: boolean= false
      }
 
      handleError(error:any){
+      if(error.status ===400 && error.error == 'title name exist already, choose an unique name'){
+        alert('Title name exist already please choose an unique name for your title!')
+      }
       if (error.statu===404) {
         console.log(error);
         
