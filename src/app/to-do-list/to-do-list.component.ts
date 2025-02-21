128542a5080727f0c7edffb29d5949ba36dff559
Your Name <you@example.com>
2025-02-21 10:16:42 +0500

    update

--- src/app/to-do-list/to-do-list.component.ts
+++ src/app/to-do-list/to-do-list.component.ts
@@ -83,7 +83,7 @@ isTaskExist: boolean= false
 
   onCreateList(){
     const token = this.authService.getToken()
-    this.apiService.addTask(this.taskDescriptionInput,this.taskTitleInput,token).subscribe({next: (item)=>{console.log(item);
+    this.apiService.addTask(this.taskTitleInput,this.taskDescriptionInput,token).subscribe({next: (item)=>{console.log(item);
       
       
     
