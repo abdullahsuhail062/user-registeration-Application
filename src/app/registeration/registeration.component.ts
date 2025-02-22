74032b2b3f40f3872313063f1d327f8cefbd3c37
Your Name <you@example.com>
2025-02-22 08:54:58 +0500

    update

--- src/app/registeration/registeration.component.ts
+++ src/app/registeration/registeration.component.ts
@@ -37,7 +37,7 @@ export class RegisterationComponent  {
 
 
 
-  constructor(private authService: AuthService,private router: Router,private apiService: ApiServiceService, private http: HttpClient){
+  constructor(private authService: AuthService,private router: Router,private apiService: ApiServiceService){
     this.signUpForm = new FormGroup({username: new FormControl('',[Validators.required,Validators.minLength(3),Validators.pattern('^[a-zA-Z0-9]*')])
       ,email: new FormControl('',[Validators.required, Validators.pattern(/^[a-zA-Z0-9._%+-]+@example\.(com|org|net)$/)]),password: new FormControl('',[Validators.required,Validators.minLength(8),
         Validators.pattern('^[a-zA-Z0-9]+$'
