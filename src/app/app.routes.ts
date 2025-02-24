9af06455f1bfeb92d7054543d85fc54d8272f641
Your Name <you@example.com>
2025-02-24 09:01:20 +0500

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,7 +6,6 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { ToDoListComponent } from './to-do-list/to-do-list.component';
-import { CalculatorComponent } from './calculator/calculator.component';
 
 export const routes: Routes = [{path: '', component: AppComponent},{path: 'registeration',component: RegisterationComponent},
-     { path: 'dashboard', component: DashboardComponent, canActivate: [authGuard] },{path: 'login', component: LoginComponent, canActivate: [loginGuard]},{ path: '', redirectTo: '/login', pathMatch: 'full' },{path: 'user-profile', component: UserProfileComponent},{path: '', component: AppComponent},{path: 'ToDoList', component: ToDoListComponent},{path: 'calculator', component: CalculatorComponent}];
+     { path: 'dashboard', component: DashboardComponent, canActivate: [authGuard] },{path: 'login', component: LoginComponent, canActivate: [loginGuard]},{ path: '', redirectTo: '/login', pathMatch: 'full' },{path: 'user-profile', component: UserProfileComponent},{path: '', component: AppComponent},{path: 'ToDoList', component: ToDoListComponent}];
